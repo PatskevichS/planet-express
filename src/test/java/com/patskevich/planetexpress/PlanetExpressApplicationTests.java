e433c739c8fe9bd58fef43a8fa94b4d6ae17be8c
PatskevichS <LuRonBel@gmail.com>
2019-04-20 17:53:14 +0300

    Initial commit

---
+++ src/test/java/com/patskevich/planetexpress/PlanetExpressApplicationTests.java
@@ -0,0 +1,16 @@
+package com.patskevich.planetexpress;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlanetExpressApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
